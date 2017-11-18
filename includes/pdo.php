b771787d1b6f14a45f54f19e1856790566884422
L. K <31963040+LordKorea@users.noreply.github.com>
2017-11-18 19:28:59 +0100

    Enclosed the whole script lifecycle in a transaction to protect against concurrent modifications

--- includes/pdo.php
+++ includes/pdo.php
@@ -7,4 +7,12 @@
         die("Database connection failed!");
     }
     unset($db_user, $db_pw, $db_host, $db_name);
+    
+    $db_handle->beginTransaction();
+    
+    function dbh_autocommit() {
+        global $db_handle;
+        $db_handle->commit();
+    }
+    register_shutdown_function('dbh_autocommit');
 ?>
