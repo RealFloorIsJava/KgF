1be90fa643d7359cab45aac6974fcffecf2e5e64
L. K <31963040+LordKorea@users.noreply.github.com>
2017-12-09 20:56:37 +0100

    added convenience include script for minify

---
+++ src/lib/minify.php
@@ -0,0 +1,13 @@
+<?php
+  // See https://github.com/matthiasmullie/minify/issues/83
+  $path = "lib";
+  require_once $path."/minify/src/Minify.php";
+  require_once $path."/minify/src/CSS.php";
+  require_once $path."/minify/src/JS.php";
+  require_once $path."/minify/src/Exception.php";
+  require_once $path."/minify/src/Exceptions/BasicException.php";
+  require_once $path."/minify/src/Exceptions/FileImportException.php";
+  require_once $path."/minify/src/Exceptions/IOException.php";
+  require_once $path."/path-converter/src/ConverterInterface.php";
+  require_once $path."/path-converter/src/Converter.php";
+?>
