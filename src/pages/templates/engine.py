80a008012df1d5b33ad0688d6c72410ccb5d935a
L. K <31963040+LordKorea@users.noreply.github.com>
2018-01-03 20:13:52 +0100

    updated copyright

--- src/pages/templates/engine.py
+++ src/pages/templates/engine.py
@@ -1,7 +1,7 @@
 """Part of KgF.
 
 MIT License
-Copyright (c) 2017 LordKorea
+Copyright (c) 2017-2018 LordKorea
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
