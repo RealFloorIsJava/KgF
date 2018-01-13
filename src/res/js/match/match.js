661258f21215bde170704ea856d0467147f17f34
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-01-13 20:16:14 +0800

    Can keep track of number of cards chosen and to be chosen

--- src/res/js/match/match.js
+++ src/res/js/match/match.js
@@ -65,6 +65,8 @@
     allowPick = data.allowPick
     isSpectator = data.isSpectator
 
+    $("#cardcount").html(numSelected + "/" + numGaps);
+
     if (isSpectator) {
       $(".match-hand").addClass("invisible")
     }
