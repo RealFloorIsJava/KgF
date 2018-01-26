8a5eb7090170aba3dd18891c4dfa71fb177f3ff0
L. K <31963040+LordKorea@users.noreply.github.com>
2018-01-26 12:05:28 +0100

    changed appearance of card counter

--- src/res/js/match/match.js
+++ src/res/js/match/match.js
@@ -66,7 +66,9 @@
     allowChoose = data.allowChoose
     allowPick = data.allowPick
     isSpectator = data.isSpectator
-    $("#cardcount").html("Selected: " + numSelected + "/" + numGaps);
+
+    $("#cardcount").html("Selected: " + numSelected + " of " + numGaps);
+    $("#cardcount").toggleClass("invisible", !allowChoose)
 
     if (isSpectator) {
       $(".match-hand").addClass("invisible")
